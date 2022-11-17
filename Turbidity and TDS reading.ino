a808d420811460b9e4cff70d4105bd131f3a9603
Yaseen <117686811+ymehasi7@users.noreply.github.com>
2022-11-17 11:28:24 +0000

    Add files via upload

--- Turbidity and TDS reading.ino
+++ Turbidity and TDS reading.ino
@@ -3,13 +3,12 @@ void setup() {
 }
 
 void loop() {
-  int turbidityValue = analogRead(36);// read the input on analog pin 36 (Turbidity):
-  int tdsValue = analogRead(0);// read the input on analog pin 0 (TDS):
+  int turbidityValue = analogRead(35);// read the input on analog pin 35 (Turbidity):
+  int tdsValue = analogRead(36);// read the input on analog pin 36 (TDS):
   float turbidityVoltage = turbidityValue * (5.0 / 4096.0); // Convert the analog reading (which goes from 0 - 4096) to a voltage (0 - 5V):
-  //float tdsVoltage = tdsValue * (5.0 / 4096.0); // Convert the TDS reading (which goes from 0 - 4096) to a voltage (0 - 5V):
   Serial.println("Turbidity Rating:");
   Serial.println(turbidityVoltage); // print out the turbidity rating:
-  Serial.println("TDS Reading:");
+  Serial.println("TDS Reading (PPM):");
   Serial.println(tdsValue); // print out the turbidity rating:
   delay(5000);
 
